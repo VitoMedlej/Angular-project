3f7f00ff717f06f7041194c7db841ebbde6880b7
Vito Medlej <rip.vitto@gmail.com>
2022-09-30 14:47:10 +0300

    building home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { HeroComponent } from './Components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    AboutComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatIconModule,
     MatListModule,
     MatToolbarModule,
-    NoopAnimationsModule
+    MatButtonModule,
+    MatDividerModule,
+    NoopAnimationsModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+      {path: 'about', component: AboutComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
